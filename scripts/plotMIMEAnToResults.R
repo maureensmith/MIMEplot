cf3ef72b94f2909220b4de43cb8aa90f01231326
Maureen Smith <maureen.smith@gmx.de>
2021-05-13 19:28:39 +0200

    ignore subfoldes which don't have MIMEAnTo result tables

--- scripts/plotMIMEAnToResults.R
+++ scripts/plotMIMEAnToResults.R
@@ -23,7 +23,7 @@ if(length(args) < 2) {
 The MIMEAnToDirectory contains the one or several MIMEAnTo result directories with output files.\n
 The reference file contains the reference sequence in a fasta file.\n
 The plots are saved in the resultDirectory.\n
-An optional argument suffix can be given, if the MIMAnTo files were saved with a suffix. \n\n")
+An optional fourth argument suffix can be given, if the MIMAnTo files were saved with a suffix. \n\n")
 
   #terminate without saving workspace
   quit("no")
@@ -74,33 +74,38 @@ evaluations <- evaluations[dir.exists(paste0(data_dir,"/",evaluations))]
 
 for(eval_dir in evaluations) {
   results_dir <- paste0(data_dir,"/",eval_dir)
-  kd.table <- read.csv(paste0(results_dir,"/PositionWiseKdEstimates",suffix,".csv"), header=T, sep="\t")
-  kd.table_all <- rbind(kd.table_all, data.frame(kd.table, comparison=eval_dir))
-  
-  max_kd.table <- read.csv(paste0(results_dir,"/PositionWiseMaxKd",suffix,".csv"), header=T, sep="\t")
-  
-  width=5
-  smoothed_median = filter(max_kd.table$median.Kd, rep(1,width)/width, sides=2)
-  smoothed_5 = filter(max_kd.table$X5..percentil, rep(1,width)/width, sides=2)
-  smoothed_95= filter(max_kd.table$X95..percentil, rep(1,width)/width, sides=2)
-  max_kd_smooth.table <- data.frame(max_kd.table, 
-                                smoothedMedian=as.vector(smoothed_median),
-                                smoothed5=as.vector(smoothed_5),
-                                smoothed95=as.vector(smoothed_95),
-                                comparison=eval_dir)
-  max_kd.table_all <- rbind(max_kd.table_all, max_kd_smooth.table)
-  #############Plot max Kd
-  #kd_sub.table <- max_kd.table_all[str_detect(max_kd.table_all$comparison, paste0(eval_dir,"$")),]
-  p<-getPlot(max_kd_smooth.table)
-  outputFile = paste0(results_dir, "/maxKd_",eval_dir,suffix,".pdf")
-  ggsave(filename = outputFile, plot=p, device = "pdf", width=40, height=10, units = "cm")
-  
-  
-  ############ Plot long plot with all Kds
-  position_range= max(kd.table$pos1, na.rm = T) - min(kd.table$pos1, na.rm = T)
-  p<-getLongPlot(kd.table, refSeq.df)
-  outputFile = paste0(results_dir, "/positionwise_Kds_",eval_dir,suffix,".pdf")
-  ggsave(filename = outputFile, plot=p, device = "pdf", width=40*position_range/100, height=10, limitsize = FALSE)
+  kd_file <- paste0(results_dir,"/PositionWiseKdEstimates",suffix,".csv")
+  max_kd_file <- paste0(results_dir,"/PositionWiseMaxKd",suffix,".csv")
+  if(file.exists(kd_file) & file.exists(max_kd_file)) {
+    kd.table <- read.csv(kd_file, header=T, sep="\t")
+    kd.table_all <- rbind(kd.table_all, data.frame(kd.table, comparison=eval_dir))
+    
+    max_kd.table <- read.csv(max_kd_file, header=T, sep="\t")
+    
+    width=5
+    smoothed_median = filter(max_kd.table$median.Kd, rep(1,width)/width, sides=2)
+    smoothed_5 = filter(max_kd.table$X5..percentil, rep(1,width)/width, sides=2)
+    smoothed_95= filter(max_kd.table$X95..percentil, rep(1,width)/width, sides=2)
+    max_kd_smooth.table <- data.frame(max_kd.table, 
+                                  smoothedMedian=as.vector(smoothed_median),
+                                  smoothed5=as.vector(smoothed_5),
+                                  smoothed95=as.vector(smoothed_95),
+                                  comparison=eval_dir)
+    max_kd.table_all <- rbind(max_kd.table_all, max_kd_smooth.table)
+    #############Plot max Kd
+    #kd_sub.table <- max_kd.table_all[str_detect(max_kd.table_all$comparison, paste0(eval_dir,"$")),]
+    p<-getPlot(max_kd_smooth.table)
+    outputFile = paste0(results_dir, "/maxKd_",eval_dir,suffix,".pdf")
+    ggsave(filename = outputFile, plot=p, device = "pdf", width=40, height=10, units = "cm")
+    
+    
+    ############ Plot long plot with all Kds
+    position_range= max(kd.table$pos1, na.rm = T) - min(kd.table$pos1, na.rm = T)
+    p<-getLongPlot(kd.table, refSeq.df)
+    outputFile = paste0(results_dir, "/positionwise_Kds_",eval_dir,suffix,".pdf")
+    ggsave(filename = outputFile, plot=p, device = "pdf", width=40*position_range/100, height=10, limitsize = FALSE)
+
+  }
 }
 
 # Plot all smoothed Kds in one Plot
